c2ac63ebb1f7e5aa5891ba5a2b3ccc8ac5ea75e2
Bart Stroeken <Bart.Stroeken@gmail.com>
2013-01-29 00:19:16 +0100

    Changed its timestamp

--- Jobs/TestJob.php
+++ Jobs/TestJob.php
@@ -6,7 +6,7 @@
 class TestJob extends CronJob
 {
 	
-	protected $time = '* * */3 * * *';
+	protected $time = '*/3 * * * * *';
 
 	protected function handle(){
 		$this->log('Here comes the magic!');
